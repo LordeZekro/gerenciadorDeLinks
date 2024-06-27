d9b9fb24a73c07eaa527bdc1d90401d9e59075dd
LordeZekro <lryouki8905@gmail.com>
2024-06-27 06:16:03 -0300

    O começo de tudo! :tada:

---
+++ script.js
@@ -0,0 +1,37 @@
+function toggleMode () {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  
+  // Isso aqui não passa de uma outra variação, porém utilizando "If" e "Else"
+  // if(html.classList.contains("light")) {
+  //   html.classList.remove("light")
+  // } else {
+  //   html.classList.add("light")
+  // }
+
+    //pegar a tag img
+    const img = document.querySelector("#profile img")
+
+    //adicionar condicional para caso esteja com light usar a foto "light"
+    if(html.classList.contains("light")) {
+      img.setAttribute("src", "./assets/avatar-light.png")
+    }
+    // Caso não esteja ele irá usar a foto padrão
+    else {
+    img.setAttribute("src", "./assets/avatar.png")
+    }
+  
+    
+    const alt =document.querySelector("#profile img")
+
+    if(html.classList.contains("light")) {
+      alt.setAttribute("alt", "Foto do Mayk Brito sorrindo, de óculos com grau em fundo colorido")
+    }
+
+    else {
+      alt.setAttribute(
+        "alt","Foto do Mayk Brito sorrindo, de óculos escuros em fundo colorido")
+    }
+
+
+}
